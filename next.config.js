b5d91b39f3648dfe90f355347529eb1032b5bb34
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-08-23 19:06:25 +0100

    Setup | Installed WebPack & Added Product Env Link Re-Routing

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
+const webpack = require('webpack')
+
 const isProd = (process.env.NODE_ENV || 'production') === 'production'
+
+const assetPrefix = isProd ? '/activity-trends' : ''
 
 module.exports = {
   exportPathMap: () => ({
     '/': { page: '/' },
+    '/page1': { page: '/page1' },
   }),
-  assetPrefix: isProd ? '/activity-trends' : '',
+  assetPrefix: assetPrefix,
+  webpack: config => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+      }),
+    )
+
+    return config
+  },
 }
