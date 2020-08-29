b7ef68f24e2408ca930443eaf0c66ebcc0add42d
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-08-29 17:28:58 +0100

    Workouts | Implemented Basic Pie Chart

--- components/Workouts.tsx
+++ components/Workouts.tsx
@@ -1,4 +1,5 @@
 import { Container } from 'react-bootstrap'
+import { PieChart, Pie, Sector, Cell } from 'recharts'
 
 interface WorkoutsProps {
     data: WorkoutData[]
@@ -22,17 +23,41 @@ export interface WorkoutData {
     'Weeather Humidity': string
 }
 
-const Workouts: React.FunctionComponent<WorkoutsProps> = ({data}) => {
+const Workouts: React.FunctionComponent<WorkoutsProps> = ({ data }) => {
     return (
         <Container>
             <p>{data.length} workouts recorded.</p>
-            <p>{extractWorkoutTypes(data)}</p>
+            <PieChart width={800} height={400}>
+                <Pie
+                    data={extractWorkoutTypes(data)}
+                    nameKey="name"
+                    dataKey="value"
+                    labelLine={false}
+                    //label={renderCustomizedLabel}
+                    outerRadius={80}
+                    fill="#8884d8"
+                >
+                </Pie>
+            </PieChart>
         </Container>
     )
 }
 
-function extractWorkoutTypes(data) {
-    return data.map(datum => {return datum["Type"]});
+function extractWorkoutTypes(data: WorkoutData[]) {
+    let counts = {};
+    const types = data.map(datum => { return datum["Type"] });
+    for (var i = 0; i < types.length; i++) {
+        var num = types[i];
+        counts[num] = (counts[num] || 0) + 1
+    }
+    const uniqueTypes = types.filter((v, i, self) => {return self.indexOf(v) === i})
+    const result = uniqueTypes.map(type => {
+        return {
+            name: type,
+            value: counts[type]
+        }
+    });
+    return result;
 }
 
 export default Workouts;
