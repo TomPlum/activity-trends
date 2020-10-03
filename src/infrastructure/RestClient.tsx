7a15ecade909b20c072b4534efd5c951662562fb
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2020-10-03 20:47:11 +0100

    Infrastructure | Implemented Basic Rest Client

---
+++ src/infrastructure/RestClient.tsx
@@ -0,0 +1,37 @@
+class RestClient {
+    private static readonly host = process.env.BACKEND_URL
+
+    static get = (endpoint: string) => RestClient.makeRestRequest("GET", undefined, endpoint);
+
+    static post = (endpoint: string, body: {}) => RestClient.makeRestRequest("POST", body, endpoint);
+    
+
+    private static async makeRestRequest(method: string, body: object, endpoint: string) {
+        if (!this.host) {
+            throw new ReferenceError('Host URI is not defined!');
+        }
+
+        if (!endpoint) {
+            throw new ReferenceError("Endpoint is not defined!");
+        }
+
+        const URI = this.host + endpoint;
+        console.log("Sending " + method + " request to " + URI);
+
+        return fetch(URI, {
+            method: method,
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: body ? JSON.stringify(body) : undefined
+        }).then(response => {
+            const json = response.json()
+            console.log("Received JSON response:\n", json);
+            return json;
+        }).catch(e => {
+            console.log("An error occurred while making a request to " + URI, e);
+        });
+    }
+}
+
+export default RestClient;
