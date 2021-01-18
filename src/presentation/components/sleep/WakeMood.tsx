25e9aabc1269576aec9618a6386e01966064539b
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-01-18 20:51:46 +0000

    Accessibility | Added WakeMood Titles

--- src/presentation/components/sleep/WakeMood.tsx
+++ src/presentation/components/sleep/WakeMood.tsx
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { Mood } from '../../../domain/Mood';
-import { Container } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGrinBeam, faSmile, faMeh, faFrown, faTired, faMehBlank, } from '@fortawesome/free-solid-svg-icons';
+import React, {Component} from "react";
+import {Mood} from '../../../domain/Mood';
+import {Container} from 'react-bootstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faFrown, faGrinBeam, faMeh, faSmile, faTired,} from '@fortawesome/free-solid-svg-icons';
 import styles from '../../../assets/sass/components/sleep/WakeMood.module.scss';
 
 interface WakeMoodProps {
@@ -13,11 +13,11 @@ class WakeMood extends Component<WakeMoodProps> {
     render() {
         return (
             <Container>
-                <FontAwesomeIcon icon={faGrinBeam} className={this.getClass(Mood.GREAT)} />
-                <FontAwesomeIcon icon={faSmile} className={this.getClass(Mood.GOOD)} />
-                <FontAwesomeIcon icon={faMeh} className={this.getClass(Mood.OK)} />
-                <FontAwesomeIcon icon={faFrown} className={this.getClass(Mood.NOT_SO_GOOD)} />
-                <FontAwesomeIcon icon={faTired} className={this.getClass(Mood.BAD)} />
+                <FontAwesomeIcon icon={faGrinBeam} title={"Great"} className={this.getClass(Mood.GREAT)} />
+                <FontAwesomeIcon icon={faSmile} title={"Good"} className={this.getClass(Mood.GOOD)} />
+                <FontAwesomeIcon icon={faMeh} title="Ok" className={this.getClass(Mood.OK)} />
+                <FontAwesomeIcon icon={faFrown} title={"Not So Good"} className={this.getClass(Mood.NOT_SO_GOOD)} />
+                <FontAwesomeIcon icon={faTired} title={"Bad"} className={this.getClass(Mood.BAD)} />
             </Container>
         );
     }
