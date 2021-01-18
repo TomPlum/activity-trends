b7ca6912da714f3460baa2bba26af9b3cc540374
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-01-18 20:01:03 +0000

    Layout | Moved GitHub Icon -> Footer & Switched Snapshot Selector & HealthInfo Positions

--- src/presentation/layout/Footer.tsx
+++ src/presentation/layout/Footer.tsx
@@ -1,7 +1,9 @@
-import { Container, Navbar } from 'react-bootstrap'
+import {Container, Nav, Navbar} from 'react-bootstrap'
 import styles from '../../assets/sass/components/layout/Footer.module.scss'
 import React, { Component } from 'react';
 import moment from "moment";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGithub} from "@fortawesome/free-brands-svg-icons";
 
 
 class Footer extends Component {
@@ -9,6 +11,11 @@ class Footer extends Component {
         return (
             <Container fluid>
                 <Navbar fixed="bottom" className={styles.footer}>
+                    <Nav>
+                        <Nav.Link target="_blank" href="https://www.github.com/TomPlum" className={styles.link}>
+                            <FontAwesomeIcon icon={faGithub} size="lg" fixedWidth className={styles.github} />
+                        </Nav.Link>
+                    </Nav>
                     <Navbar.Brand>
                         <span>Thomas Plumpton &copy; {Footer.getCurrentYear()}</span>
                     </Navbar.Brand>
--- src/presentation/layout/Header.tsx
+++ src/presentation/layout/Header.tsx
@@ -1,7 +1,5 @@
-import { Navbar, Nav } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faGithub } from '@fortawesome/free-brands-svg-icons'
-import React, { Component } from 'react';
+import {Navbar} from 'react-bootstrap';
+import React, {Component} from 'react';
 import SnapshotSelector from './SnapshotSelector';
 import HealthInfo from "./HealthInfo";
 import styles from '../../assets/sass/components/layout/Header.module.scss'
@@ -23,13 +21,8 @@ class Header extends Component {
                 <Navbar.Toggle />
 
                 <Navbar.Collapse className="justify-content-end">
-                    <HealthInfo />
                     <SnapshotSelector />
-                    <Nav>
-                        <Nav.Link target="_blank" href="https://www.github.com/TomPlum" className={styles.link}>
-                            <FontAwesomeIcon icon={faGithub} size="lg" fixedWidth className={styles.github} />
-                        </Nav.Link>
-                    </Nav>
+                    <HealthInfo />
                 </Navbar.Collapse>
             </Navbar>
         );
