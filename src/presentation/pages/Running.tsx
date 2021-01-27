e83d3d6fc467451c1a3d69c860b45325e6b99fd2
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-01-27 21:45:52 +0000

    Presentation | Added Running Page

--- src/presentation/pages/Running.tsx
+++ src/presentation/pages/Running.tsx
@@ -1,11 +1,36 @@
 import React, { Component } from "react";
 import { Container } from "react-bootstrap";
+import LoadingSpinner from "../layout/LoadingSpinner";
+import DisabledOverlay from "../layout/DisabledOverlay";
+import { CardioSessions } from "../../domain/health/workout/CardioSessions";
+import { HealthService } from "../../application/service/HealthService";
 
-class Running extends Component {
+interface RunningState {
+  data: CardioSessions;
+  loading: boolean;
+}
+
+class Running extends Component<{}, RunningState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: undefined,
+      loading: true
+    }
+  }
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const data = await new HealthService().getOutdoorExercise();
+    this.setState({ loading: false, data });
+  }
+
   render() {
+    const { data, loading } = this.state;
     return (
-      <Container fluid>
-        <p>This is the running page.</p>
+      <Container fluid className={"page-container"}>
+        <LoadingSpinner active={loading}/>
+        <DisabledOverlay active={!loading && !data}/>
+        {data}
       </Container>
     );
   }
