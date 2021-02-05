9ced500aa3339c7db8b0c7e1b88b461d51f9c29d
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-02-05 20:45:59 +0000

    Infrastructure | Added Get Workout Route -> HealthDataRepository

--- src/infrastructure/repository/HealthDataRepository.ts
+++ src/infrastructure/repository/HealthDataRepository.ts
@@ -1,9 +1,21 @@
-import { WorkoutSessionResponse } from "../types/Health";
+import {WorkoutRouteResponse, WorkoutSessionResponse} from "../types/Health";
 import RestClient from "../RestClient";
 
 export class HealthDataRepository {
   async getCardioSessions(): Promise<WorkoutSessionResponse> {
-    const response = await RestClient.get<WorkoutSessionResponse>('/workouts/sessions');
+    const response = await RestClient.get<WorkoutSessionResponse>("/workouts/sessions");
+
+    if (response.errors && response.errors.length > 0) {
+      throw new Error(response.errors[0].message);
+    }
+
+    if (response.data) {
+      return response.data;
+    }
+  }
+
+  async getWorkoutSessionRoute(name: string): Promise<WorkoutRouteResponse> {
+    const response = await RestClient.get<WorkoutRouteResponse>("/workouts/route/" + name);
 
     if (response.errors && response.errors.length > 0) {
       throw new Error(response.errors[0].message);
--- src/tests/infrastructure/repositories/HealthDataRepository.test.ts
+++ src/tests/infrastructure/repositories/HealthDataRepository.test.ts
@@ -1,6 +1,6 @@
-import { HealthDataRepository } from "../../../infrastructure/repository/HealthDataRepository";
+import {HealthDataRepository} from "../../../infrastructure/repository/HealthDataRepository";
 import RestClient from "../../../infrastructure/RestClient";
-import { WorkoutSessionResponse } from "../../../infrastructure/types/Health";
+import {WorkoutRouteResponse, WorkoutSessionResponse} from "../../../infrastructure/types/Health";
 
 describe("Health Data Repository", () => {
 
@@ -10,15 +10,15 @@ describe("Health Data Repository", () => {
   jest.mock("../../../infrastructure/RestClient")
   RestClient.get = mockGet
 
-  describe("Get Running Data", () => {
+  describe("Get Cardio Sessions", () => {
     it("Should call the Rest Client with the correct endpoint", () => {
-      whenCallApiReturnValidResponse();
+      whenCallApiReturnValidResponse(getValidWorkoutData());
       repository.getCardioSessions();
       expect(mockGet).toHaveBeenCalledWith("/workouts/sessions")
     });
 
-    it("Should call return the response data if present", () => {
-      whenCallApiReturnValidResponse();
+    it("Should return the response data if present", () => {
+      whenCallApiReturnValidResponse(getValidWorkoutData());
       const response = repository.getCardioSessions();
       expect(response).toEqual(getValidWorkoutData());
     });
@@ -26,21 +26,7 @@ describe("Health Data Repository", () => {
     it("Should throw an error with the caught errors message", () => {
       whenCallApiReturnError();
       expect(() => repository.getCardioSessions()).rejects.toThrow("Something went wrong.");
-    })
-
-    function whenCallApiReturnValidResponse() {
-      mockGet.mockImplementationOnce(() => Promise.resolve({
-        data: getValidWorkoutData(),
-        errors: []
-      }));
-    }
-
-    function whenCallApiReturnError() {
-      mockGet.mockImplementationOnce(() => Promise.resolve({
-        data: Promise.resolve(undefined),
-        errors: [new Error("Something went wrong.")]
-      }));
-    }
+    });
 
     function getValidWorkoutData(): Promise<WorkoutSessionResponse> {
       return Promise.resolve({
@@ -50,9 +36,72 @@ describe("Health Data Repository", () => {
           distance: "0.0",
           energyBurned: "177.234",
           startTime: "2017-10-02T19:54:13",
-          endTime: "2017-10-02T20:10:35"
+          endTime: "2017-10-02T20:10:35",
+          meta: {
+            timeZone: "Europe/London",
+            temperature: {
+              value: 53,
+              unit: "DEGREES_FAHRENHEIT",
+              humidity: 8300
+            }
+          }
         }],
       });
     }
   });
+
+  describe("Get Workout Session Route", () => {
+    it("Should call the Rest Client with the correct endpoint", () => {
+      whenCallApiReturnValidResponse(getValidRouteResponse());
+      repository.getWorkoutSessionRoute("example-route-name");
+      expect(mockGet).toHaveBeenCalledWith("/workouts/route/example-route-name");
+    });
+
+    it("Should return the response data if present", () => {
+      whenCallApiReturnValidResponse(getValidRouteResponse());
+      const response = repository.getWorkoutSessionRoute("example-route-name");
+      expect(response).toEqual(getValidRouteResponse());
+    });
+
+    it("Should throw an error with the caught errors message", () => {
+      whenCallApiReturnError();
+      expect(() => repository.getWorkoutSessionRoute("route-name")).rejects.toThrow("Something went wrong.");
+    });
+
+    function getValidRouteResponse(): Promise<WorkoutRouteResponse> {
+      return Promise.resolve({
+        creationDate: "2020-08-23T17:40:03",
+        source: {
+          name: "Apple Health Export",
+          version: "1.1"
+        },
+        route: {
+          points: [
+            {
+              lat: 53.252968,
+              long: -2.538252
+            },
+            {
+              lat: 53.252965,
+              long: -2.538253
+            },
+          ]
+        }
+      });
+    }
+  });
+
+  function whenCallApiReturnValidResponse(data) {
+    mockGet.mockImplementationOnce(() => Promise.resolve({
+      data: data,
+      errors: []
+    }));
+  }
+
+  function whenCallApiReturnError() {
+    mockGet.mockImplementationOnce(() => Promise.resolve({
+      data: Promise.resolve(undefined),
+      errors: [new Error("Something went wrong.")]
+    }));
+  }
 });
