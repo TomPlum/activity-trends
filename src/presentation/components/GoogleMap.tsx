e774c3abe718320710cc749eee98c7d0bc979fb4
TomPlum <Thomas.Plumpton@Hotmail.co.uk>
2021-02-11 19:37:26 +0000

    Components | Renamed MapContainer -> GoogleMap & Added Wrapping Div

--- src/presentation/components/GoogleMap.tsx
+++ src/presentation/components/GoogleMap.tsx
@@ -1,6 +1,7 @@
 import {GoogleApiWrapper, IProvidedProps, Map} from 'google-maps-react';
 import styles from '../../assets/sass/components/GoogleMap.module.scss'
 import React from "react";
+import {Container} from "react-bootstrap";
 
 interface GoogleMapProps extends IProvidedProps {
     center?: { lat: number, lng: number };
@@ -9,7 +10,9 @@ interface GoogleMapProps extends IProvidedProps {
 export class GoogleMap extends React.Component<GoogleMapProps> {
     render() {
         return (
-            <Map google={this.props.google}>{this.props.children}</Map>
+            <Container className={styles.container}>
+                <Map google={this.props.google}>{this.props.children}</Map>
+            </Container>
         );
     }
 }
